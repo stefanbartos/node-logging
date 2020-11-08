0eec46a057513089379e7940a3369f1ce5e911cc
Štefan Bartoš <>
2020-11-08 21:24:38 +0100

    basic logging node server

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const parser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+app.disable('x-powered-by');
+
+app.use(parser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-Requested-With,content-type'
+    );
+
+    next();
+});
+
+app.post('/log', (req, res) => {
+    const time = new Date().toLocaleTimeString();
+    console.log(`${time}  request come`);
+
+    // console.log(req.headers)
+    // console.log(req.body)
+    const data = JSON.stringify(req.body);
+    console.log(data)
+    fs.appendFile('output.txt', data + '\n', (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    });
+
+    res.send('ok');
+});
+
+app.listen(3000, () => {
+    console.log('server stared on port: 3000');
+});
