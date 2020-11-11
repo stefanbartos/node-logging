9ceb8b7e37d457a97dd119bed6511e2dd99c992d
Štefan Bartoš <>
2020-11-11 13:36:36 +0100

    added output_log folder,add IP segregation

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const parser = require('body-parser');
 const fs = require('fs');
 
 const PORT = 3000;
+const LOG_DIRECTORY_PATH = './output_log';
 
 const app = express();
 app.disable('x-powered-by');
 
 app.use(parser.json());
-app.use((req, res, next) => {
+app.use((_, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Methods',
@@ -22,20 +23,45 @@ app.use((req, res, next) => {
     next();
 });
 
+const checkOutputLogDirectory = () => {
+    if (!fs.existsSync(LOG_DIRECTORY_PATH)) {
+        fs.mkdirSync(LOG_DIRECTORY_PATH);
+    }
+};
+
+checkOutputLogDirectory();
+
+const getShortDate = (dateToFormat) => {
+    const day = `0${dateToFormat.getDate()}`.slice(-2);
+    const month = `0${dateToFormat.getMonth() + 1}`.slice(-2);
+    const year = dateToFormat.getFullYear();
+
+    return `${day}.${month}.${year}`;
+};
+
+let date = getShortDate(new Date());
+
+setInterval(() => {
+    date = getShortDate(new Date());
+}, 1000 * 60 * 5);
+
 app.post('/log', (req, res) => {
     const time = new Date().toLocaleTimeString();
-    console.log(`${time}  request come`);
+    console.log(`${time} - Request from ${req.ip}`);
 
-    // console.log(req.headers)
+    // console.log(req)
     // console.log(req.body)
     const data = JSON.stringify(req.body);
-    console.log(data)
-    fs.appendFile('output.txt', data + '\n', (err) => {
-        if (err) {
-            console.error(err);
-            return;
+    // console.log(data)
+    fs.appendFile(
+        `${LOG_DIRECTORY_PATH}/${req.ip}-${date}.log`,
+        data + '\n',
+        (err) => {
+            if (err) {
+                console.error(err);
+            }
         }
-    });
+    );
 
     res.send('ok');
 });
